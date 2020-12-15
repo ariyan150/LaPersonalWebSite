1204430ef0ff4545e5e4f72929b4b0cd83e1ed2d
Ariyan Abidi <a.abidi150@gmail.com>
2020-12-15 23:05:39 +0330

    deploy

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9wa3#t1i%(yw=f_*q7($m%dj$27+0&^$(d9it_(aoea8yb0pbk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lapersonalweb.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
